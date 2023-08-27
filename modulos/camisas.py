edac152b276cfc39a2232c92a714361637611792
Gian <giansantos.dev@gmail.com>
2023-08-27 00:35:18 -0300

    adicionada funcionalidade de adicionar ao carrinho

--- modulos/camisas.py
+++ modulos/camisas.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QDialog
+from modulos.carrinho_funcs import add_to_carrinho
 from template.paginainicial import Ui_Inicial
 from template.camisas import Ui_Camisas
+
 
 class paginacamisas(QDialog):
     def __init__(self, tela_inicial, *args, **argvs):
@@ -8,7 +10,10 @@ class paginacamisas(QDialog):
         self.ui = Ui_Camisas()
         self.ui.setupUi(self)
         self.ui.botao_voltar.clicked.connect(self.voltando)
+        self.ui.botao_add_camisa_branca_naruto.clicked.connect(
+            lambda: add_to_carrinho(1))
         self.tela_inicial = tela_inicial
+
     def voltando(self):
         self.window = self.tela_inicial.show()
         self.clearMask()
