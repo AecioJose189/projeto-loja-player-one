af5e5ba917f2b28e01377b1d4e6b582b639c339f
Gian <giansantos.dev@gmail.com>
2023-08-27 23:10:24 -0300

    corrigido bug do carrinho voltar para o login

--- modulos/paginainicial.py
+++ modulos/paginainicial.py
@@ -12,7 +12,7 @@ from modulos.carrinho import PaginaCarrinho
 
 
 class paginainicial(QDialog):
-    def __init__(self, tela_inicial, *args, **argvs):
+    def __init__(self, tela_login, *args, **argvs):
         super(paginainicial, self).__init__(*args, **argvs)
         self.ui = Ui_Inicial()
         self.ui.setupUi(self)
@@ -23,14 +23,14 @@ class paginainicial(QDialog):
         self.ui.botao_chapeus.clicked.connect(self.chapeus)
         self.ui.botao_moletons.clicked.connect(self.moletom)
         self.ui.botao_carrinho.clicked.connect(self.carrinho)
-        self.tela_inicial = tela_inicial
+        self.tela_login = tela_login
 
     def sair(self):
         current_dir = os.path.dirname(__file__)
         with open(os.path.join(current_dir, '..', 'db', 'sessao.json'), 'w') as sessao_arquivo:
             sessao_arquivo.write('')
 
-        self.window = self.tela_inicial.show()
+        self.window = self.tela_login.show()
         self.clearMask()
         self.destroy()
 
@@ -60,6 +60,6 @@ class paginainicial(QDialog):
         self.hide()
 
     def carrinho(self):
-        self.window = PaginaCarrinho(self.tela_inicial)
+        self.window = PaginaCarrinho(self)
         self.window.show()
         self.hide()
